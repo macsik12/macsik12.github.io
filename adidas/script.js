e44ccbe989bf843287c515f79b1e0223b9bc6842
macsik12 <58954077+macsik12@users.noreply.github.com>
2020-05-27 17:33:39 +0300

    Add files via upload

--- adidas/script.js
+++ adidas/script.js
@@ -4,7 +4,10 @@
 
 let li_lang = document.getElementById('languages_li');
 let list_lang = document.getElementById('languages_list')
+let country = 'USA';
+let country_span = document.getElementById('country');
 let langIsOpen = false;
+let currentCountryP = document.querySelector('.header .languages_li *.selected');
 
 function showLangPanel() {
     list_lang.style.transform = 'scaleY(1)';
@@ -29,5 +32,16 @@ li_lang.addEventListener('click', function(event) {
 document.body.addEventListener('click', function(event) {
     if(!event.target.closest('#languages_li')) {
         closeLangPanel()
+    }
+})
+
+list_lang.addEventListener('click', function(event) {
+    if(event.target.dataset.t == 'lan') {
+        currentCountryP.classList.remove('selected')
+        country = event.target.textContent;
+        country_span.innerHTML = event.target.textContent;
+        event.target.classList.add('selected')
+        currentCountryP = event.target;
+        setTimeout(closeLangPanel, 0)
     }
 })
